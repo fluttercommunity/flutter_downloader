ce017df192f9eb29c0ead1deeadd53c58c2b749d
WangXinhai <18907191365@189.cn>
2019-08-13 15:59:52 +0800

    merge two methods and check null when used
    merge `getOpenFileIntent` and `validateIntent` validatedFileIntent

--- android/src/main/java/vn/hunghd/flutterdownloader/IntentUtils.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/IntentUtils.java
@@ -14,24 +14,62 @@ import android.util.Log;
 import android.os.Build;
 
 public class IntentUtils {
-    public static synchronized Intent getOpenFileIntent(Context context, String path, String contentType) {
-        File file = new File(path);
+    
+    private static Intent buildIntent(Context context, File file, String mime){
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        if (Build.VERSION.SDK_INT >= 24) {
-            Uri uri = FileProvider.getUriForFile(
-                    context,
-                    context.getPackageName() + ".flutter_downloader.provider", file);
-            intent.setDataAndType(uri, contentType);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri uri = FileProvider.getUriForFile(context, context.getPackageName() + ".flutter_downloader.provider", file);
+            intent.setDataAndType(uri, mime);
         } else {
-            intent.setDataAndType(Uri.fromFile(file), contentType);
+            intent.setDataAndType(Uri.fromFile(file), mime);
         }
-
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        return intent;
+        if(validateIntent(context, intent)
+            return intent;
+        return null;
     }
 
-    public static synchronized boolean validateIntent(Context context, Intent intent) {
+    public static synchronized Intent validatedFileIntent(Context context, String path, String contentType) {
+        File file = new File(path);
+        Intent intent = buildIntent(context,file,contentType);
+        if(validateIntent(context, intent))
+            return intent;
+        String mime = null;
+        try {
+            FileInputStream inputFile = new FileInputStream(path);
+            mime = URLConnection.guessContentTypeFromStream(inputFile);
+            if(mime !=null){
+                mime = URLConnection.guessContentTypeFromStream(new BufferedInputStream(inputFile));
+            }
+        } catch (Exception ignored){
+            
+        }
+        if(mime!=null) {
+            intent = buildIntent(context,file,mime);
+            if(validateIntent(context, intent))
+                return intent;
+        }
+        return null;
+//     }
+//     public static synchronized Intent getOpenFileIntent(Context context, String path, String contentType) {
+//         File file = new File(path);
+//         Intent intent = new Intent(Intent.ACTION_VIEW);
+//         if (Build.VERSION.SDK_INT >= 24) {
+//             Uri uri = FileProvider.getUriForFile(
+//                     context,
+//                     context.getPackageName() + ".flutter_downloader.provider", file);
+//             intent.setDataAndType(uri, contentType);
+//         } else {
+//             intent.setDataAndType(Uri.fromFile(file), contentType);
+//         }
+
+//         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//         return intent;
+//     }
+
+    private static boolean validateIntent(Context context, Intent intent) {
         PackageManager manager = context.getPackageManager();
         List<ResolveInfo> infos = manager.queryIntentActivities(intent, 0);
         if (infos.size() > 0) {
