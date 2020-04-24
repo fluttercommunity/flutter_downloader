4d4e3565c9e4eb5d1795961407cdca109ce174ca
Marcel Garus <marcel.garus@gmail.com>
2020-04-24 18:39:15 +0200

    cast objects explicitly

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -11,9 +11,9 @@ void dispatchCallback() {
     final Function callback = PluginUtilities.getCallbackFromHandle(
         CallbackHandle.fromRawHandle(args[0]));
 
-    final String id = args[1];
-    final int status = args[2];
-    final double progress = (args[3] as int) / 100.0;
+    final id = args[1] as String;
+    final status = args[2] as int;
+    final progress = (args[3] as int).toDouble() / 100.0;
 
     callback(id, status, progress);
   });
