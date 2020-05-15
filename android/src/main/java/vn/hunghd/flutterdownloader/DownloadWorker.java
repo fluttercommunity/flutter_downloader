7dcdfe136292f3bd0615e296ff578ea70f3d3f39
Valeriy Bodak <valery.bodak@exposit.com>
2020-05-15 11:13:37 +0300

    Increased timeout to 45 seconds

--- android/src/main/java/vn/hunghd/flutterdownloader/DownloadWorker.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/DownloadWorker.java
@@ -263,8 +263,8 @@ public class DownloadWorker extends Worker implements MethodChannel.MethodCallHa
                 log("Open connection to " + url);
                 httpConn = (HttpURLConnection) resourceUrl.openConnection();
 
-                httpConn.setConnectTimeout(15000);
-                httpConn.setReadTimeout(15000);
+                httpConn.setConnectTimeout(45000);
+                httpConn.setReadTimeout(45000);
                 httpConn.setInstanceFollowRedirects(false);   // Make the logic below easier to detect redirections
                 httpConn.setRequestProperty("User-Agent", "Mozilla/5.0...");
 
