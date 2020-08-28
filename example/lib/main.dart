def7f374b44bd07e87bef99db6198e196e8656bd
Marcel Garus <marcel.garus@gmail.com>
2020-08-28 13:24:17 +0200

    Restructure example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,6 +9,42 @@ import 'package:permission_handler/permission_handler.dart';
 
 const debug = true;
 
+/// The concept of a file in this app.
+/// Don't confuse this with dart:io's File.
+class File {
+  File(this.name, this.url)
+      : assert(name != null),
+        assert(url != null);
+
+  final String name;
+  final String url;
+  DownloadTask task; // Represents a download. Is mutable and null by default.
+}
+
+/// A list of example files that can be downloaded by this app.
+final _files = [
+  File('Learning Android Studio',
+      'http://barbra-coco.dyndns.org/student/learning_android_studio.pdf'),
+  File('Android Programming Cookbook',
+      'http://enos.itcollege.ee/~jpoial/allalaadimised/reading/Android-Programming-Cookbook.pdf'),
+  File('iOS Programming Guide',
+      'http://darwinlogic.com/uploads/education/iOS_Programming_Guide.pdf'),
+  File('Objective-C Programming (Pre-Course Workbook)',
+      'https://www.bignerdranch.com/documents/objective-c-prereading-assignment.pdf'),
+  File('Arches National Park',
+      'https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg'),
+  File('Canyonlands National Park',
+      'https://upload.wikimedia.org/wikipedia/commons/7/78/Canyonlands_National_Park%E2%80%A6Needles_area_%286294480744%29.jpg'),
+  File('Death Valley National Park',
+      'https://upload.wikimedia.org/wikipedia/commons/b/b2/Sand_Dunes_in_Death_Valley_National_Park.jpg'),
+  File('Gates of the Arctic National Park and Preserve',
+      'https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg'),
+  File('Big Buck Bunny',
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'),
+  File('Elephant Dream',
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+];
+
 void main() async {
   await FlutterDownloader.initialize(debug: debug);
   runApp(MyApp());
@@ -20,109 +56,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Downloader Example',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: StartScreen(),
+      home: StartPage(),
     );
   }
 }
 
-class StartScreen extends StatelessWidget {
+/// This page introduces the user into the concept of the app.
+class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Downloader Example')),
-      body: Center(
-        child: Text("We're gonna download some files."),
-      ),
+      body: Center(child: Text("We're gonna download some files.")),
       floatingActionButton: ContinueButton(),
     );
   }
 }
 
 class ContinueButton extends StatelessWidget {
-  Future<void> _continue(BuildContext context) async {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      label: Text('Continue'),
+      onPressed: () => _onClick(context),
+    );
+  }
+
+  Future<void> _onClick(BuildContext context) async {
+    // Ensure that the storage permission is granted on Android.
     if (Platform.isAndroid && !await Permission.storage.request().isGranted) {
       context.scaffold.showSnackBar(SnackBar(
         content: Text('You need to grant storage permission so that we '
             'can download files. 😇'),
       ));
+      return;
     }
 
-    // Permission granted.
+    // Load the existing tasks from the DB and bind matching [File]s to them.
+    for (final existingTask in await FlutterDownloader.loadTasks()) {
+      // Find the [File] that corresponds to the task. If there is one, set its
+      // tasks to the already existing one.
+      final task = _files.singleWhere((task) => task.url == existingTask.url,
+          orElse: () => null);
+      task?.task = existingTask;
+    }
+
+    // Push the new page.
+    // Note: The new page also uses the [_files]. In production, you probably
+    // don't want to solve this with global state, but rather pass an array of
+    // files or something similar to the other widget.
     context.navigator.pushReplacement(MaterialPageRoute(
       builder: (_) => DownloadList(),
     ));
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      onPressed: () => _continue(context),
-      label: Text('Continue'),
-    );
-  }
 }
 
-class DownloadList extends StatefulWidget {
-  @override
-  _DownloadListState createState() => _DownloadListState();
-}
-
-class _DownloadListState extends State<DownloadList> {
-  final _files = [
-    File('Learning Android Studio',
-        'http://barbra-coco.dyndns.org/student/learning_android_studio.pdf'),
-    File('Android Programming Cookbook',
-        'http://enos.itcollege.ee/~jpoial/allalaadimised/reading/Android-Programming-Cookbook.pdf'),
-    File('iOS Programming Guide',
-        'http://darwinlogic.com/uploads/education/iOS_Programming_Guide.pdf'),
-    File('Objective-C Programming (Pre-Course Workbook)',
-        'https://www.bignerdranch.com/documents/objective-c-prereading-assignment.pdf'),
-    File('Arches National Park',
-        'https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg'),
-    File('Canyonlands National Park',
-        'https://upload.wikimedia.org/wikipedia/commons/7/78/Canyonlands_National_Park%E2%80%A6Needles_area_%286294480744%29.jpg'),
-    File('Death Valley National Park',
-        'https://upload.wikimedia.org/wikipedia/commons/b/b2/Sand_Dunes_in_Death_Valley_National_Park.jpg'),
-    File('Gates of the Arctic National Park and Preserve',
-        'https://upload.wikimedia.org/wikipedia/commons/e/e4/GatesofArctic.jpg'),
-    File('Big Buck Bunny',
-        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'),
-    File('Elephant Dream',
-        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
-  ];
-
-  void initState() {
-    super.initState();
-    scheduleMicrotask(() async {
-      for (final existingTask in await FlutterDownloader.loadTasks()) {
-        final task = _files.singleWhere((task) => task.url == existingTask.url,
-            orElse: () => null);
-        task?.task = existingTask;
-      }
-    });
-  }
-
+/// This page shows the downloadable files.
+class DownloadList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Downloader Example')),
-      body: ListView(
-        children: <Widget>[
-          for (final file in _files) FileWidget(file),
-        ],
-      ),
+      body: ListView(children: _files.map((file) => FileWidget(file)).toList()),
     );
   }
 }
 
-class File {
-  File(this.name, this.url);
-
-  final String name;
-  final String url;
-  DownloadTask task;
-}
-
+/// Widget that shows a [File] in the list.
 class FileWidget extends StatefulWidget {
   const FileWidget(this.file);
 
