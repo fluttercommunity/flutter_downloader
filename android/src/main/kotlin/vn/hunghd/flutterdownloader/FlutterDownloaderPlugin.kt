cc1146ec1f5324bc178cbf9accf9e0312059d98a
rekire <git@reki.re>
2022-09-12 21:20:06 +0200

    Add missing imports and fix some null value issues

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/FlutterDownloaderPlugin.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/FlutterDownloaderPlugin.kt
@@ -1,11 +1,26 @@
 package vn.hunghd.flutterdownloader
 
+import android.content.ContentResolver
+import android.content.ContentUris
 import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.*
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.*
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import java.io.File
+import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
+class FlutterDownloaderPlugin : MethodChannel.MethodCallHandler, FlutterPlugin {
     private var flutterChannel: MethodChannel? = null
     private var taskDao: TaskDao? = null
     private var context: Context? = null
@@ -15,20 +30,20 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     private var ignoreSsl = 0
     private val initializationLock = Any()
 
-    fun onAttachedToEngine(applicationContext: Context?, messenger: BinaryMessenger?) {
+    fun onAttachedToEngine(applicationContext: Context?, messenger: BinaryMessenger) {
         synchronized(initializationLock) {
             if (flutterChannel != null) {
                 return
             }
             context = applicationContext
             flutterChannel = MethodChannel(messenger, CHANNEL)
-            flutterChannel.setMethodCallHandler(this)
+            flutterChannel?.setMethodCallHandler(this)
             val dbHelper: TaskDbHelper = TaskDbHelper.Companion.getInstance(context)
             taskDao = TaskDao(dbHelper)
         }
     }
 
-    fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when(call.method) {
             "initialize" ->initialize(call, result)
             "registerCallback" ->registerCallback(call, result)
@@ -46,17 +61,17 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
         }
     }
 
-    fun onAttachedToEngine(binding: FlutterPluginBinding) {
-        onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger())
+    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+        onAttachedToEngine(binding.applicationContext, binding.getBinaryMessenger())
     }
 
-    fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
         context = null
-        if (flutterChannel != null) {
-            flutterChannel.setMethodCallHandler(null)
-            flutterChannel = null
-        }
+        flutterChannel?.setMethodCallHandler(null)
+        flutterChannel = null
     }
+
+    private fun requireContext() = requireNotNull(context)
 
     private fun buildRequest(
         url: String?, savedDir: String?, filename: String?, headers: String?,
@@ -102,7 +117,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
         args["task_id"] = id
         args["status"] = status
         args["progress"] = progress
-        flutterChannel.invokeMethod("updateProgress", args)
+        flutterChannel?.invokeMethod("updateProgress", args)
     }
 
     private fun initialize(call: MethodCall, result: MethodChannel.Result) {
@@ -123,20 +138,24 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
         result.success(null)
     }
 
+    private fun <T> MethodCall.requireArgument(key: String): T = requireNotNull(argument(key)) {
+        "Required key '$key' was null"
+    }
+
     private fun enqueue(call: MethodCall, result: MethodChannel.Result) {
-        val url: String = call.argument("url")
-        val savedDir: String = call.argument("saved_dir")
-        val filename: String = call.argument("file_name")
-        val headers: String = call.argument("headers")
-        val showNotification: Boolean = call.argument("show_notification")
-        val openFileFromNotification: Boolean = call.argument("open_file_from_notification")
-        val requiresStorageNotLow: Boolean = call.argument("requires_storage_not_low")
-        val saveInPublicStorage: Boolean = call.argument("save_in_public_storage")
+        val url: String = call.requireArgument("url")
+        val savedDir: String = call.requireArgument("saved_dir")
+        val filename: String? = call.argument("file_name")
+        val headers: String = call.requireArgument("headers")
+        val showNotification: Boolean = call.requireArgument("show_notification")
+        val openFileFromNotification: Boolean = call.requireArgument("open_file_from_notification")
+        val requiresStorageNotLow: Boolean = call.requireArgument("requires_storage_not_low")
+        val saveInPublicStorage: Boolean = call.requireArgument("save_in_public_storage")
         val request: WorkRequest = buildRequest(
             url, savedDir, filename, headers, showNotification,
             openFileFromNotification, false, requiresStorageNotLow, saveInPublicStorage
         )
-        WorkManager.getInstance(context).enqueue(request)
+        WorkManager.getInstance(requireContext()).enqueue(request)
         val taskId: String = request.id.toString()
         result.success(taskId)
         sendUpdateProgress(taskId, DownloadStatus.ENQUEUED, 0)
@@ -149,9 +168,9 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     private fun loadTasks(call: MethodCall, result: MethodChannel.Result) {
         val tasks = taskDao!!.loadAllTasks()
         val array: MutableList<Map<*, *>> = ArrayList()
-        for (task in tasks!!) {
+        for (task in tasks) {
             val item: MutableMap<String, Any?> = HashMap()
-            item["task_id"] = task!!.taskId
+            item["task_id"] = task.taskId
             item["status"] = task.status
             item["progress"] = task.progress
             item["url"] = task.url
@@ -164,12 +183,12 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     }
 
     private fun loadTasksWithRawQuery(call: MethodCall, result: MethodChannel.Result) {
-        val query: String = call.argument("query")
+        val query: String = call.requireArgument("query")
         val tasks = taskDao!!.loadTasksWithRawQuery(query)
         val array: MutableList<Map<*, *>> = ArrayList()
-        for (task in tasks!!) {
+        for (task in tasks) {
             val item: MutableMap<String, Any?> = HashMap()
-            item["task_id"] = task!!.taskId
+            item["task_id"] = task.taskId
             item["status"] = task.status
             item["progress"] = task.progress
             item["url"] = task.url
@@ -182,30 +201,30 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     }
 
     private fun cancel(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
-        WorkManager.getInstance(context).cancelWorkById(UUID.fromString(taskId))
+        val taskId: String = call.requireArgument("task_id")
+        WorkManager.getInstance(requireContext()).cancelWorkById(UUID.fromString(taskId))
         result.success(null)
     }
 
     private fun cancelAll(call: MethodCall, result: MethodChannel.Result) {
-        WorkManager.getInstance(context).cancelAllWorkByTag(TAG)
+        WorkManager.getInstance(requireContext()).cancelAllWorkByTag(TAG)
         result.success(null)
     }
 
     private fun pause(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
+        val taskId: String = call.requireArgument("task_id")
         // mark the current task is cancelled to process pause request
         // the worker will depends on this flag to prepare data for resume request
         taskDao!!.updateTask(taskId, true)
         // cancel running task, this method causes WorkManager.isStopped() turning true and the download loop will be stopped
-        WorkManager.getInstance(context).cancelWorkById(UUID.fromString(taskId))
+        WorkManager.getInstance(requireContext()).cancelWorkById(UUID.fromString(taskId))
         result.success(null)
     }
 
     private fun resume(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
+        val taskId: String = call.requireArgument("task_id")
         val task = taskDao!!.loadTask(taskId)
-        val requiresStorageNotLow: Boolean = call.argument("requires_storage_not_low")
+        val requiresStorageNotLow: Boolean = call.requireArgument("requires_storage_not_low")
         if (task != null) {
             if (task.status == DownloadStatus.PAUSED) {
                 var filename = task.filename
@@ -230,7 +249,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
                         task.progress,
                         false
                     )
-                    WorkManager.getInstance(context).enqueue(request)
+                    WorkManager.getInstance(requireContext()).enqueue(request)
                 } else {
                     taskDao!!.updateTask(taskId, false)
                     result.error(
@@ -248,9 +267,9 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     }
 
     private fun retry(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
+        val taskId: String = call.requireArgument("task_id")
         val task = taskDao!!.loadTask(taskId)
-        val requiresStorageNotLow: Boolean = call.argument("requires_storage_not_low")
+        val requiresStorageNotLow: Boolean = call.requireArgument("requires_storage_not_low")
         if (task != null) {
             if (task.status == DownloadStatus.FAILED || task.status == DownloadStatus.CANCELED) {
                 val request: WorkRequest = buildRequest(
@@ -258,7 +277,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
                     task.headers, task.showNotification, task.openFileFromNotification,
                     false, requiresStorageNotLow, task.saveInPublicStorage
                 )
-                val newTaskId: String = request.getId().toString()
+                val newTaskId: String = request.id.toString()
                 result.success(newTaskId)
                 sendUpdateProgress(newTaskId, DownloadStatus.ENQUEUED, task.progress)
                 taskDao!!.updateTask(
@@ -268,7 +287,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
                     task.progress,
                     false
                 )
-                WorkManager.getInstance(context).enqueue(request)
+                WorkManager.getInstance(requireContext()).enqueue(request)
             } else {
                 result.error("invalid_status", "only failed and canceled task can be retried", null)
             }
@@ -278,7 +297,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     }
 
     private fun open(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
+        val taskId: String = call.requireArgument("task_id")
         val task = taskDao!!.loadTask(taskId)
         if (task != null) {
             if (task.status == DownloadStatus.COMPLETE) {
@@ -286,13 +305,13 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
                 val savedDir = task.savedDir
                 var filename = task.filename
                 if (filename == null) {
-                    filename = fileURL!!.substring(fileURL.lastIndexOf("/") + 1, fileURL.length)
+                    filename = fileURL.substring(fileURL.lastIndexOf("/") + 1, fileURL.length)
                 }
                 val saveFilePath = savedDir + File.separator + filename
                 val intent: Intent? =
-                    IntentUtils.validatedFileIntent(context, saveFilePath, task.mimeType)
+                    IntentUtils.validatedFileIntent(requireContext(), saveFilePath, task.mimeType)
                 if (intent != null) {
-                    context.startActivity(intent)
+                    requireContext().startActivity(intent)
                     result.success(true)
                 } else {
                     result.success(false)
@@ -306,12 +325,12 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
     }
 
     private fun remove(call: MethodCall, result: MethodChannel.Result) {
-        val taskId: String = call.argument("task_id")
-        val shouldDeleteContent: Boolean = call.argument("should_delete_content")
+        val taskId: String = call.requireArgument("task_id")
+        val shouldDeleteContent: Boolean = call.requireArgument("should_delete_content")
         val task = taskDao!!.loadTask(taskId)
         if (task != null) {
             if (task.status == DownloadStatus.ENQUEUED || task.status == DownloadStatus.RUNNING) {
-                WorkManager.getInstance(context).cancelWorkById(UUID.fromString(taskId))
+                WorkManager.getInstance(requireContext()).cancelWorkById(UUID.fromString(taskId))
             }
             if (shouldDeleteContent) {
                 var filename = task.filename
@@ -326,7 +345,7 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
                 }
             }
             taskDao!!.deleteTask(taskId)
-            NotificationManagerCompat.from(context).cancel(task.primaryId)
+            NotificationManagerCompat.from(requireContext()).cancel(task.primaryId)
             result.success(null)
         } else {
             result.error("invalid_task_id", "not found task corresponding to given task id", null)
@@ -335,37 +354,27 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
 
     private fun deleteFileInMediaStore(file: File) {
         // Set up the projection (we only need the ID)
-        val projection = arrayOf<String>(MediaStore.Images.Media._ID)
+        val projection = arrayOf(MediaStore.Images.Media._ID)
 
         // Match on the file path
         val imageSelection: String = MediaStore.Images.Media.DATA + " = ?"
-        val videoSelection: String = MediaStore.Video.Media.DATA + " = ?"
-        val selectionArgs = arrayOf<String>(file.getAbsolutePath())
+        val selectionArgs = arrayOf<String>(file.absolutePath)
 
         // Query for the ID of the media matching the file path
         val imageQueryUri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        val videoQueryUri: Uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-        val contentResolver: ContentResolver = context.getContentResolver()
+        val contentResolver: ContentResolver = requireContext().contentResolver
 
         // search the file in image store first
-        val imageCursor: Cursor =
-            contentResolver.query(imageQueryUri, projection, imageSelection, selectionArgs, null)
+        val imageCursor = contentResolver.query(imageQueryUri, projection, imageSelection, selectionArgs, null)
         if (imageCursor != null && imageCursor.moveToFirst()) {
             // We found the ID. Deleting the item via the content provider will also remove the file
             val id: Long =
                 imageCursor.getLong(imageCursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID))
-            val deleteUri: Uri =
-                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            val deleteUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
             contentResolver.delete(deleteUri, null, null)
         } else {
             // File not found in image store DB, try to search in video store
-            val videoCursor: Cursor = contentResolver.query(
-                imageQueryUri,
-                projection,
-                imageSelection,
-                selectionArgs,
-                null
-            )
+            val videoCursor = contentResolver.query(imageQueryUri, projection, imageSelection, selectionArgs, null)
             if (videoCursor != null && videoCursor.moveToFirst()) {
                 // We found the ID. Deleting the item via the content provider will also remove the file
                 val id: Long =
@@ -376,9 +385,9 @@ class FlutterDownloaderPlugin : MethodCallHandler, FlutterPlugin {
             } else {
                 // can not find the file in media store DB at all
             }
-            if (videoCursor != null) videoCursor.close()
+            videoCursor?.close()
         }
-        if (imageCursor != null) imageCursor.close()
+        imageCursor?.close()
     }
 
     companion object {
