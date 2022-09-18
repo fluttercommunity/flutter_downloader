27ebe3b0c61dd93a8ac63f329ba7884217b0459d
rekire <git@reki.re>
2022-09-18 21:22:42 +0200

    Add way to isolate tests

--- test/example_test.dart
+++ test/example_test.dart
@@ -4,19 +4,26 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('example test', () async {
-    var testDir = await Directory.systemTemp.createTemp('test');
-    Directory.current = testDir;
-    print(Directory.current.path);
+  test('example.com download', isolated((testDir) async {
     const sampleUrl = 'https://www.example.com';
     const sampleUrlHash = '740e7397907c0b004010d92b33d283e98f74063d';
-    try {
-      final example = await FlutterDownloader().startDownload(sampleUrl);
+      await FlutterDownloader().startDownload(sampleUrl);
       final metaFile = File('${testDir.path}/$sampleUrlHash.meta');
       assert(metaFile.existsSync());
+  }),);
+}
+
+/// Run a test in a temp directory, which will be deleted after the execution.
+dynamic Function() isolated(Future<dynamic> Function(Directory testDir) body) {
+  return () async {
+    final originalDirectory = Directory.current;
+    final testDir = await Directory.systemTemp.createTemp('test');
+    Directory.current = testDir;
+    try {
+      return await body(testDir);
     } finally {
-      Directory.current = testDir.parent;
+      Directory.current = originalDirectory;
       await testDir.delete(recursive: true);
     }
-  });
+  };
 }
