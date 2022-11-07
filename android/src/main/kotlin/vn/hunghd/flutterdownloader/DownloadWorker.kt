e82837b5ab55107196596c234078e646d3a02bb9
jackleemeta <jackleemeta@outlook.com>
2022-11-07 20:11:47 +0800

    fix: android wrong last progress due to missing initial value

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
@@ -277,6 +277,9 @@ class DownloadWorker(context: Context, params: WorkerParameters) :
         var timeout = timeout
         visited = HashMap()
         try {
+            val task = taskDao?.loadTask(id.toString())
+            if (task != null) lastProgress = task.progress;
+
             // handle redirection logic
             while (true) {
                 if (!visited.containsKey(url)) {
