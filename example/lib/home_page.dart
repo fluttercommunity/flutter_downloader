7f3c1be7c40ec15b0108af6904d38d5c2841ba30
Bartek Pacia <barpac02@gmail.com>
2022-11-17 18:22:38 +0100

    Clean up example app code (#759)
    * example app: simplify permission code
    
    * code refactors
    
    * remove not-needed requestLegacyExternalStorage from Manifest
    
    * add "save to public storage" checkbox to example app
    
    * clean up example app home page code

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -3,7 +3,7 @@ import 'dart:isolate';
 import 'dart:ui';
 
 import 'package:android_path_provider/android_path_provider.dart';
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_downloader_example/data.dart';
@@ -25,8 +25,9 @@ class MyHomePage extends StatefulWidget with WidgetsBindingObserver {
 class _MyHomePageState extends State<MyHomePage> {
   List<TaskInfo>? _tasks;
   late List<ItemHolder> _items;
-  late bool _loading;
+  late bool _showContent;
   late bool _permissionReady;
+  late bool _saveInPublicStorage;
   late String _localPath;
   final ReceivePort _port = ReceivePort();
 
@@ -38,8 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     FlutterDownloader.registerCallback(downloadCallback, step: 1);
 
-    _loading = true;
+    _showContent = false;
     _permissionReady = false;
+    _saveInPublicStorage = false;
 
     _prepare();
   }
@@ -100,54 +102,70 @@ class _MyHomePageState extends State<MyHomePage> {
         ?.send([id, status, progress]);
   }
 
-  Widget _buildDownloadList() => ListView(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        children: [
-          for (final item in _items)
-            item.task == null
-                ? _buildListSectionHeading(item.name!)
-                : DownloadListItem(
-                    data: item,
-                    onTap: (task) async {
-                      final success = await _openDownloadedFile(task);
-                      if (!success) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Cannot open this file'),
-                          ),
-                        );
-                      }
-                    },
-                    onActionTap: (task) {
-                      if (task.status == DownloadTaskStatus.undefined) {
-                        _requestDownload(task);
-                      } else if (task.status == DownloadTaskStatus.running) {
-                        _pauseDownload(task);
-                      } else if (task.status == DownloadTaskStatus.paused) {
-                        _resumeDownload(task);
-                      } else if (task.status == DownloadTaskStatus.complete ||
-                          task.status == DownloadTaskStatus.canceled) {
-                        _delete(task);
-                      } else if (task.status == DownloadTaskStatus.failed) {
-                        _retryDownload(task);
-                      }
-                    },
-                    onCancel: _delete,
-                  ),
-        ],
-      );
-
-  Widget _buildListSectionHeading(String title) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.blue,
-          fontSize: 18,
+  Widget _buildDownloadList() {
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: _saveInPublicStorage,
+              onChanged: (newValue) {
+                setState(() => _saveInPublicStorage = newValue ?? false);
+              },
+            ),
+            const Text('Save in public storage'),
+          ],
         ),
-      ),
+        ..._items.map(
+          (item) {
+            final task = item.task;
+            if (task == null) {
+              return Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text(
+                  item.name!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ),
+                ),
+              );
+            }
+
+            return DownloadListItem(
+              data: item,
+              onTap: (task) async {
+                final success = await _openDownloadedFile(task);
+                if (!success) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Cannot open this file'),
+                    ),
+                  );
+                }
+              },
+              onActionTap: (task) {
+                if (task.status == DownloadTaskStatus.undefined) {
+                  _requestDownload(task);
+                } else if (task.status == DownloadTaskStatus.running) {
+                  _pauseDownload(task);
+                } else if (task.status == DownloadTaskStatus.paused) {
+                  _resumeDownload(task);
+                } else if (task.status == DownloadTaskStatus.complete ||
+                    task.status == DownloadTaskStatus.canceled) {
+                  _delete(task);
+                } else if (task.status == DownloadTaskStatus.failed) {
+                  _retryDownload(task);
+                }
+              },
+              onCancel: _delete,
+            );
+          },
+        ),
+      ],
     );
   }
 
@@ -198,7 +216,7 @@ class _MyHomePageState extends State<MyHomePage> {
       url: task.link!,
       headers: {'auth': 'test_for_sql_encoding'},
       savedDir: _localPath,
-      saveInPublicStorage: true,
+      saveInPublicStorage: _saveInPublicStorage,
     );
   }
 
@@ -216,12 +234,13 @@ class _MyHomePageState extends State<MyHomePage> {
     task.taskId = newTaskId;
   }
 
-  Future<bool> _openDownloadedFile(TaskInfo? task) {
-    if (task != null) {
-      return FlutterDownloader.open(taskId: task.taskId!);
-    } else {
-      return Future.value(false);
+  Future<bool> _openDownloadedFile(TaskInfo? task) async {
+    final taskId = task?.taskId;
+    if (taskId == null) {
+      return false;
     }
+
+    return FlutterDownloader.open(taskId: taskId);
   }
 
   Future<void> _delete(TaskInfo task) async {
@@ -238,23 +257,22 @@ class _MyHomePageState extends State<MyHomePage> {
       return true;
     }
 
-    final deviceInfo = DeviceInfoPlugin();
-    final androidInfo = await deviceInfo.androidInfo;
-    if (widget.platform == TargetPlatform.android &&
-        androidInfo.version.sdkInt <= 28) {
-      final status = await Permission.storage.status;
-      if (status != PermissionStatus.granted) {
-        final result = await Permission.storage.request();
-        if (result == PermissionStatus.granted) {
-          return true;
-        }
-      } else {
+    if (Platform.isAndroid) {
+      final info = await DeviceInfoPlugin().androidInfo;
+      if (info.version.sdkInt > 28) {
         return true;
       }
-    } else {
-      return true;
+
+      final status = await Permission.storage.status;
+      if (status == PermissionStatus.granted) {
+        return true;
+      }
+
+      final result = await Permission.storage.request();
+      return result == PermissionStatus.granted;
     }
-    return false;
+
+    throw StateError('unknown platform');
   }
 
   Future<void> _prepare() async {
@@ -326,31 +344,32 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     _permissionReady = await _checkPermission();
-
     if (_permissionReady) {
       await _prepareSaveDir();
     }
 
     setState(() {
-      _loading = false;
+      _showContent = true;
     });
   }
 
   Future<void> _prepareSaveDir() async {
-    _localPath = (await _findLocalPath())!;
+    _localPath = (await _getSavedDir())!;
     final savedDir = Directory(_localPath);
-    final hasExisted = savedDir.existsSync();
-    if (!hasExisted) {
+    if (!savedDir.existsSync()) {
       await savedDir.create();
     }
   }
 
-  Future<String?> _findLocalPath() async {
+  Future<String?> _getSavedDir() async {
     String? externalStorageDirPath;
+
     if (Platform.isAndroid) {
       try {
         externalStorageDirPath = await AndroidPathProvider.downloadsPath;
-      } catch (e) {
+      } catch (err, st) {
+        print('failed to get downloads path: $err, $st');
+
         final directory = await getExternalStorageDirectory();
         externalStorageDirPath = directory?.path;
       }
@@ -389,7 +408,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Builder(
         builder: (context) {
-          if (_loading) {
+          if (!_showContent) {
             return const Center(child: CircularProgressIndicator());
           }
 
