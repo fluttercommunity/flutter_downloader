e078c72e8ea28e1795a9d20449a3dccd209b6aa0
rekire <git@reki.re>
2023-01-18 21:33:33 +0100

    Make downloads working in iOS

--- ios/Classes/SwiftFlutterDownloaderPlugin.swift
+++ ios/Classes/SwiftFlutterDownloaderPlugin.swift
@@ -3,22 +3,47 @@ import UIKit
 import BackgroundTasks
 import os.log
 
-public class SwiftFlutterDownloaderPlugin: NSObject, FlutterPlugin {
+public class SwiftFlutterDownloaderPlugin: NSObject, FlutterPlugin, URLSessionDelegate, URLSessionDownloadDelegate {
+  private var downloadSize = [String:Int64]()
+  private var progress = [String:Int64]()
+  private var backChannel = [String:FlutterMethodChannel]()
+  private static var binaryMessenger : FlutterBinaryMessenger?;
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "fluttercommunity/flutter_downloader", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterDownloaderPlugin()
+    binaryMessenger = registrar.messenger()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   private func getCacheDir(call: FlutterMethodCall, result: @escaping FlutterResult) {
     //let group = call.arguments as! String
+
+    NSLog("Before\nAfter")
     let documentDirPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).map(\.path).first
     result(documentDirPath)
   }
 
   private func resume(call: FlutterMethodCall, result: @escaping FlutterResult) {
     let urlHash = call.arguments as! String
-    os_log("urlHash with %@", log: log, type: .debug, urlHash)
+    print("Resume download with hash \(urlHash)...")
+    backChannel[urlHash] = FlutterMethodChannel(name: "fluttercommunity/flutter_downloader/\(urlHash)", binaryMessenger: SwiftFlutterDownloaderPlugin.binaryMessenger!)
+    let config = URLSessionConfiguration.background(withIdentifier: urlHash)
+    //config.timeoutIntervalForResource = Downloader.resourceTimeout
+    let urlSession = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+
+    var request = URLRequest(url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!)
+
+    let headers = [String:String]() //TODO read header files
+    for (key, value) in headers {
+      request.setValue(value, forHTTPHeaderField: key)
+    }
+    let urlSessionDownloadTask = urlSession.downloadTask(with: request)
+    urlSessionDownloadTask.taskDescription = urlHash
+
+    // now start the task
+    urlSessionDownloadTask.resume()
+    print("Download started")
 
     result(nil)
   }
@@ -35,5 +60,55 @@ public class SwiftFlutterDownloaderPlugin: NSObject, FlutterPlugin {
       default:
         result(FlutterMethodNotImplemented)
     }
+  }
+
+  public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+    if totalBytesExpectedToWrite == NSURLSessionTransferSizeUnknown {return}
+
+    let urlHash = downloadTask.taskDescription!
+      
+    //print("callback url \(urlHash)")
+
+    if downloadSize[urlHash] == nil {
+        // TODO update size
+        print("TODO update file size")
+        backChannel[urlHash]?.invokeMethod("updateSize", arguments: totalBytesExpectedToWrite)
+        downloadSize[urlHash] = totalBytesExpectedToWrite
+        progress[urlHash] = 0
+    }
+
+    let permille = (totalBytesWritten * 1000) / totalBytesExpectedToWrite;
+
+    if progress[urlHash]! != permille {
+      progress[urlHash] = permille
+        backChannel[urlHash]?.invokeMethod("updateProgress", arguments: permille)
+        print("Update progress \(Double(permille) / 10.0)%")
+    }
+
+    if permille == 1000 {
+      print("done")
+    }
+  }
+
+  /// Process end of downloadTask sent by the urlSession.
+  ///
+  /// If successful, (over)write file to final destination per FlutterDownloadTask info
+  public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+      print("foobar")
+    let urlHash = downloadTask.taskDescription!
+
+    guard let response = downloadTask.response as? HTTPURLResponse
+    else {
+      //os_log("Could not find task associated with native id %d, or did not get HttpResponse", log: log,  type: .info, downloadTask.taskIdentifier)
+      return}
+    if response.statusCode == 404 {
+      print("status: notFound")
+      return
+    }
+    if !(200...206).contains(response.statusCode)   {
+      print("status: failed")
+      return
+    }
+      print("status: done")
   }
 }
