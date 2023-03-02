46f00b8bb501646653a5eca2f2a7c4fa4c380a1d
jackleemeta <jackleemeta@outlook.com>
2023-03-03 00:13:31 +0800

    chore: remove redundant judgment  `if(openDatabaseResult == SQLITE_OK)`, because the front lines return when `openDatabaseResult != SQLITE_OK`

--- ios/Classes/DBManager.m
+++ ios/Classes/DBManager.m
@@ -117,86 +117,86 @@
         }
         return;
     }
-    if(openDatabaseResult == SQLITE_OK) {
-        if (debug) {
-            NSLog(@"open DB successfully");
-        }
-
-        // Declare a sqlite3_stmt object in which will be stored the query after having been compiled into a SQLite statement.
-        sqlite3_stmt *compiledStatement;
-
-        // Load all data from database to memory.
-        int prepareStatementResult = sqlite3_prepare_v2(sqlite3Database, query, -1, &compiledStatement, NULL);
-        if(prepareStatementResult == SQLITE_OK) {
-            // Check if the query is non-executable.
-            if (!queryExecutable){
-                // In this case data must be loaded from the database.
+    
+    if (debug) {
+        NSLog(@"open DB successfully");
+    }
+    
+    // Declare a sqlite3_stmt object in which will be stored the query after having been compiled into a SQLite statement.
+    sqlite3_stmt *compiledStatement;
+    
+    // Load all data from database to memory.
+    int prepareStatementResult = sqlite3_prepare_v2(sqlite3Database, query, -1, &compiledStatement, NULL);
+    if(prepareStatementResult == SQLITE_OK) {
+        // Check if the query is non-executable.
+        if (!queryExecutable){
+            // In this case data must be loaded from the database.
+            
+            // Declare an array to keep the data for each fetched row.
+            NSMutableArray *arrDataRow;
+            
+            // Loop through the results and add them to the results array row by row.
+            while(sqlite3_step(compiledStatement) == SQLITE_ROW) {
+                // Initialize the mutable array that will contain the data of a fetched row.
+                arrDataRow = [[NSMutableArray alloc] init];
                 
-                // Declare an array to keep the data for each fetched row.
-                NSMutableArray *arrDataRow;
+                // Get the total number of columns.
+                int totalColumns = sqlite3_column_count(compiledStatement);
                 
-                // Loop through the results and add them to the results array row by row.
-                while(sqlite3_step(compiledStatement) == SQLITE_ROW) {
-                    // Initialize the mutable array that will contain the data of a fetched row.
-                    arrDataRow = [[NSMutableArray alloc] init];
+                // Go through all columns and fetch each column data.
+                for (int i=0; i<totalColumns; i++){
+                    // Convert the column data to text (characters).
+                    char *dbDataAsChars = (char *)sqlite3_column_text(compiledStatement, i);
                     
-                    // Get the total number of columns.
-                    int totalColumns = sqlite3_column_count(compiledStatement);
-                    
-                    // Go through all columns and fetch each column data.
-                    for (int i=0; i<totalColumns; i++){
-                        // Convert the column data to text (characters).
-                        char *dbDataAsChars = (char *)sqlite3_column_text(compiledStatement, i);
-                        
-                        // If there are contents in the currenct column (field) then add them to the current row array.
-                        if (dbDataAsChars != NULL) {
-                            // Convert the characters to string.
-                            [arrDataRow addObject:[NSString  stringWithUTF8String:dbDataAsChars]];
-                        }
-                        
-                        // Keep the current column name.
-                        if (self.arrColumnNames.count != totalColumns) {
-                            dbDataAsChars = (char *)sqlite3_column_name(compiledStatement, i);
-                            [self.arrColumnNames addObject:[NSString stringWithUTF8String:dbDataAsChars]];
-                        }
+                    // If there are contents in the currenct column (field) then add them to the current row array.
+                    if (dbDataAsChars != NULL) {
+                        // Convert the characters to string.
+                        [arrDataRow addObject:[NSString  stringWithUTF8String:dbDataAsChars]];
                     }
                     
-                    // Store each fetched data row in the results array, but first check if there is actually data.
-                    if (arrDataRow.count > 0) {
-                        [self.arrResults addObject:arrDataRow];
+                    // Keep the current column name.
+                    if (self.arrColumnNames.count != totalColumns) {
+                        dbDataAsChars = (char *)sqlite3_column_name(compiledStatement, i);
+                        [self.arrColumnNames addObject:[NSString stringWithUTF8String:dbDataAsChars]];
                     }
                 }
-            }
-            else {
-                // This is the case of an executable query (insert, update, ...).
                 
-                // Execute the query.
-                int executeQueryResults = sqlite3_step(compiledStatement);
-                if (executeQueryResults == SQLITE_DONE) {
-                    // Keep the affected rows.
-                    self.affectedRows = sqlite3_changes(sqlite3Database);
-                    
-                    // Keep the last inserted row ID.
-                    self.lastInsertedRowID = sqlite3_last_insert_rowid(sqlite3Database);
-                }
-                else {
-                    // If could not execute the query show the error message on the debugger.
-                    if (debug) {
-                        NSLog(@"DB Error: %s", sqlite3_errmsg(sqlite3Database));
-                    }
+                // Store each fetched data row in the results array, but first check if there is actually data.
+                if (arrDataRow.count > 0) {
+                    [self.arrResults addObject:arrDataRow];
                 }
             }
         }
         else {
-            // In the database cannot be opened then show the error message on the debugger.
-            if (debug) {
-                NSLog(@"%s", sqlite3_errmsg(sqlite3Database));
+            // This is the case of an executable query (insert, update, ...).
+            
+            // Execute the query.
+            int executeQueryResults = sqlite3_step(compiledStatement);
+            if (executeQueryResults == SQLITE_DONE) {
+                // Keep the affected rows.
+                self.affectedRows = sqlite3_changes(sqlite3Database);
+                
+                // Keep the last inserted row ID.
+                self.lastInsertedRowID = sqlite3_last_insert_rowid(sqlite3Database);
+            }
+            else {
+                // If could not execute the query show the error message on the debugger.
+                if (debug) {
+                    NSLog(@"DB Error: %s", sqlite3_errmsg(sqlite3Database));
+                }
             }
         }
-        
-        // Release the compiled statement from memory.
-        sqlite3_finalize(compiledStatement);
     }
+    else {
+        // In the database cannot be opened then show the error message on the debugger.
+        if (debug) {
+            NSLog(@"%s", sqlite3_errmsg(sqlite3Database));
+        }
+    }
+    
+    // Release the compiled statement from memory.
+    sqlite3_finalize(compiledStatement);
+    
     
     // Close the database.
     sqlite3_close(sqlite3Database);
