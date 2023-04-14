f0e3357c0ff97970c0972bf69fd6b41f23f7b0de
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-04-14 14:05:08 +0100

    Check protocol when opening connection without ignoreSsl (#848)

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
@@ -300,7 +300,11 @@ class DownloadWorker(context: Context, params: WorkerParameters) :
                         resourceUrl.openConnection() as HttpURLConnection
                     }
                 } else {
-                    resourceUrl.openConnection() as HttpsURLConnection
+                    if (resourceUrl.protocol.lowercase(Locale.US) == "https") {
+                        resourceUrl.openConnection() as HttpsURLConnection
+                    } else {
+                        resourceUrl.openConnection() as HttpURLConnection
+                    }
                 }
                 log("Open connection to $url")
                 httpConn.connectTimeout = timeout
