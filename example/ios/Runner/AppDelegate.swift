abc8e0cec09b750cd204553f199529ccde6672f7
anhhtbk <anhhtbk@gmail.com>
2023-10-16 23:18:58 +0700

    fix bug "A C function pointer can only be formed"

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import flutter_downloader
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      GeneratedPluginRegistrant.register(with: self)
+      FlutterDownloaderPlugin.setPluginRegistrantCallback { registry in
+              if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+                 FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+              }
+      }
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
